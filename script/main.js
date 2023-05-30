b38efc7d971a15945bf8a003231fb62480bb1e09
Andriy Dyachyk <andriy.dyachyk98@gmail.com>
2023-05-30 18:27:30 +0200

    creata generazione randomica numeri

--- script/main.js
+++ script/main.js
@@ -1,1 +1,11 @@
 console.log('principi.js loaded');
+
+
+let simonsArray = []
+
+for (let i = 0; simonsArray.length<5; i++) {
+    simonsArray.push(Math.floor(Math.random() * 9));
+    console.log(simonsArray)
+    document.getElementById('simon_sais').innerText = simonsArray.join(' ');
+}
+
